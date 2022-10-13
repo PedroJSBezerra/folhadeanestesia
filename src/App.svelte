742efc5cdaaec196980075027d04928b4e4bd78e
PedroJSBezerra <52613930+PedroJSBezerra@users.noreply.github.com>
2022-10-13 13:21:56 -0300

    1 file modified, 4 added

--- src/App.svelte
+++ src/App.svelte
@@ -1,131 +1,98 @@
-<script>
-</script>
+<section>
+  <h6>
+    TERMO DE CONSENTIMENTO INFORMADO PARA PROCEDIMENTOS INVASIVOS E CIRURGIAS
+  </h6>
 
-<button on:click={() => window.print()}>IMPRIMIR</button>
+  <p>
+    Autorizo o Dr. (a)_________________________________________ a realizar na minha pessoa,
+    o (s) seguinte (s) procedimento (s) invasivo [s) ou cirurgia (5):____________________________________________
+  <p>
+    Autorizo o mesmo a ter uma cópia do relatório cirúrgico respeitados os critérios de confidencialidade e sigilo impostos pela
+    legislação vigente.
+  </p>
+  <p>
+    A proposta do procedimento me foram explicadas claramente. Tive oportunidade de fazer perguntas, que foram respondidas
+    satisfatoriamente. Entendo que não exista garantia absoluta sobre os resultados a serem obtidos. Entre as complicações
+    potenciais e os riscos pode-se citar especialmente:________________________________________________________
+  </p>
 
-<main>
-  <header>
-    <div class="logo">Logo da Empresa</div>
-    <h1>FOLHA DE ANESTESIA</h1>
-  </header>
+  <p>
+    De acordo:
+  </p>
+  <p>
+    [ ] Paciente [ ] Responsável
+  </p>
+  <p>
+    Nome legível: _____________________________________________________
+  </p>
+  <p>
+    Assinatura: ____________________________________________
+  </p>
+</section>
 
-  <table class="table table-bordered border-dark fw-bold">
-     <tbody>
-      <tr>
-        <td colspan="4"><p>UNIDADE / SERVIÇO</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>LEITO</p><p contenteditable="true"></p></td>
-        <td colspan="2"><p>N° PRONTUÁRIO</p><p contenteditable="true"></p></td>
-      </tr>
-      <tr>
-        <td colspan="4"><p>NOME</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>IDADE</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>SEXO</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>COR</p><p contenteditable="true"></p></td>
-      </tr>
-      <tr>
-        <td colspan="1"><p>DATA</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>PRES. ARTERIAL</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>PULSO</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>RESPIRAÇÃO</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>TEMPERATURA</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>PESO</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>ALTURA</p><p contenteditable="true"></p></td>
-      </tr>
-      <tr>
-        <td colspan="1" rowspan="2"><p>TIPO SNAGUÍNEO</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>HEMÁCIAS</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>HEMOGLOBINA</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>HEMATÓCRITO</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>GLICEMIA</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>URÉIA</p><p contenteditable="true"></p></td>
-        <td colspan="1" rowspan="2"><p>OUTROS</p><p contenteditable="true"></p></td>
-      </tr>
-      <tr>
-        <td colspan="5"><p>URINA</p><p contenteditable="true"></p></td>
-      </tr>
-      <tr>
-        <td colspan="5"><p>AP. RESPIRTÓRIO</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>ASMA</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>BRONQUITE</p><p contenteditable="true"></p></td>
-      </tr>
-      <tr>
-        <td colspan="5"><p>AP. CIRCULATÓRIO</p><p contenteditable="true"></p></td>
-        <td colspan="2"><p>ELETROCARDIOGRAMA</p><p contenteditable="true"></p></td>
-      </tr>
-      <tr>
-        <td colspan="3"><p>AP. DIGESTIVO</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>DENTES</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>PESCOÇO</p><p contenteditable="true"></p></td>
-        <td colspan="2"><p>AP. URINÁRIO</p><p contenteditable="true"></p></td>
-      </tr>
-      <tr>
-        <td colspan="3"><p>ESTADO MENTAL</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>ATARAXICOS</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>CORTICOIDES</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>ALERGIA</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>HIPOTENSORES</p><p contenteditable="true"></p></td>
-      </tr>
-      <tr>
-        <td colspan="5"><p>DIAGNÓSTICO PRÉ-OPERATÓRIO</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>ESTADO FÍSICO</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>RISCO</p><p contenteditable="true"></p></td>
-      </tr>
-      <tr>
-        <td colspan="7"><p>ANESTESIAS ANTERIORES</p><p contenteditable="true"></p></td>
-      </tr>
-      <tr>
-        <td colspan="3"><p>MEDICAÇÃO PRÉ ANESTÉSICA</p><p contenteditable="true"></p></td>
-        <td colspan="1"><p>APLICADA ÀS</p><p contenteditable="true"></p></td>
-        <td colspan="3"><p>EFEITO</p><p contenteditable="true"></p></td>
-      </tr>
-    </tbody>
-  </table>
+<section>
+  <h6>TERMO DE CONSENTIMENTO INFORMADO PARA PROCEDIMENTOS ANESTÉSICOS</h6>
 
-</main>
+  Autorizo o Dr. (a) a realizar na minha pessoa,
+  anestesia/sedação para o seguinte procedimento
 
-<style>
+  A proposta do procedimento anestésico/sedação a que serei submetido (a), seus benefícios, riscos, complicações
+  potenciais e alternativas me foram explicadas claramente. Tive oportunidade de fazer perguntas, que foram respondidas
+  satisfatoriamente. Entendo que não exista garantia absoluta sobre os resultados a serem obtidos.
 
-  main {
-    max-width: 900px;
-    margin: 0 auto;
-  }
-  header {
-    display: flex;
-    flex-direction: column;
-  }
+  Entre as complicações potenciais e os riscos pode-se citar especialmente os seguintes: dor ou desconforto após 0,
+  término da anestesia, náuseas e/ou vômitos, lesão a veias e/ou artérias, reações alérgicas, infecção arterial, sensação |
+  residual da anestesia, frio ou tremores ao despertar, complicações da transfusão sanguínea, vermelhidão em partes do |
+  corpo, perda da sensação e/ou da função das extremidades do corpo, dano ao cérebro e perda de vida. Complicações
+  e/ou riscos adicionais conforme o tipo de anestesia:
 
-  header .logo {
-    max-width: 25%;
-  }
+  [] Anestesia Geral: dor de garganta, rouquidão, dano e lesão e aos dentes e às vias aéreas, consciência durante
+  anestesia, lesão aos olhos, febre, hipertermia maligna ou cegueira.
+  [1] Anestesia Regional: necessidade de mudança para anestesia geral, dor de cabeça ou sensação de anestesia residual..
 
-  header h1 {
-    font-size: 1rem;
-  }
+  [ ] Anestesia Local com Sedação: dor de garganta, rouquidão, dano e lesão aos dentes e às vias aéreas ou necessidade.
+  de mudança para anestesia geral.
 
-  tr {
-    height: .5rem;
-  }
+  [ ] Outros:
+</section>
 
-  td {
-    padding: 2px 0 0 2px;
-  }
+<section>
+  <h6>DEVE SER PREENCHIDO PELO PACIENTE OU RESPONSÁVEL</h6>
+  Autorizo qualquer outro procedimento, exame, tratamento e/ou cirurgia, incluindo transfusão de sangue e
+  hemocomponentes, em situações imprevistas que possam ocorrer e necessitem de cuidados diferentes daqueles
+  inicialmente propostos. Autorizo que qualquer órgão ou tecido removido ou cirurgicamente seja encaminhado para exames
+  complementares desde que necessário para o esclarecimento diagnóstico ou tratamento. Autorizo também a realização de
+  fotos e/ou vídeos, para documentação, que poderão ser utilizados com finalidades científicas, obedecendo-se os critérios
+  de confidencialidade e sigilos impostos da legislação vigente.
 
-  td p:nth-child(1) {
-    /* position: absolute; */
-    /* height: 14px; */
-    font-size: .5rem;
-    top: 0;
-    left: 2px;
-    word-break: break-all;
-    margin: 0;
-  }
+  Confirmo que recebi explicações, li, compreendi e concordo com os itens acima referidos e que me foi dada a oportunidade
+  para anular quaisquer espaços em branco, parágrafos ou palavras com os quais não concordasse e também entendi que a
+  qualquer momento e sem necessidade de dar nenhuma explicação poderei revogar este consentimento, antes que o
+  procedimento se realize.
 
-  td p:nth-child(2) {
-    font-size: 1rem;
-    top: 0;
-    left: 2px;
-    word-break: break-all;
-    margin: 0;
-    font-weight: 600;
-  }
-</style>
+  [ ] Paciente [ ] Responsável
+  Nome legível:
+  
+
+  Grau de Parentesco: Identidade:
+
+  Assinatura:
+  Rio de Janeiro: / / Hora:
+
+
+</section>
+
+<section>
+  <h6>DEVE SER PREENCHIDO PELO ANESTESISTA E CIRURGIÃO</h6>
+
+  Expliquei todo procedimento anestésico /sedativo ao paciente acima identificado, ou ao ser responsável, sobre os benefícios,
+  riscos e alternativas, tendo respondido as perguntas formuladas pelos mesmos. De acordo com o meu entendimento, o
+  paciente e/ou responsável, está em condições de compreender o que lhes foi informado.
+
+  Rio de Janeiro / / Hora:
+  Cirurgião (carimbo e assinatura) Anestesiologista [carimbo e assinatura)
+  Revogacão: Rj. / Lo Hora:
+
+  Paciente ou Responsável 7 Representante legal:
+  
+</section>
---
+++ src/abandonado.svelte
@@ -0,0 +1,131 @@
+<script>
+</script>
+
+<button on:click={() => window.print()}>IMPRIMIR</button>
+
+<main>
+  <header>
+    <div class="logo">Logo da Empresa</div>
+    <h1>FOLHA DE ANESTESIA</h1>
+  </header>
+
+  <table class="table table-bordered border-dark fw-bold">
+     <tbody>
+      <tr>
+        <td colspan="4"><p>UNIDADE / SERVIÇO</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>LEITO</p><p contenteditable="true"></p></td>
+        <td colspan="2"><p>N° PRONTUÁRIO</p><p contenteditable="true"></p></td>
+      </tr>
+      <tr>
+        <td colspan="4"><p>NOME</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>IDADE</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>SEXO</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>COR</p><p contenteditable="true"></p></td>
+      </tr>
+      <tr>
+        <td colspan="1"><p>DATA</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>PRES. ARTERIAL</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>PULSO</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>RESPIRAÇÃO</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>TEMPERATURA</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>PESO</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>ALTURA</p><p contenteditable="true"></p></td>
+      </tr>
+      <tr>
+        <td colspan="1" rowspan="2"><p>TIPO SNAGUÍNEO</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>HEMÁCIAS</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>HEMOGLOBINA</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>HEMATÓCRITO</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>GLICEMIA</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>URÉIA</p><p contenteditable="true"></p></td>
+        <td colspan="1" rowspan="2"><p>OUTROS</p><p contenteditable="true"></p></td>
+      </tr>
+      <tr>
+        <td colspan="5"><p>URINA</p><p contenteditable="true"></p></td>
+      </tr>
+      <tr>
+        <td colspan="5"><p>AP. RESPIRTÓRIO</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>ASMA</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>BRONQUITE</p><p contenteditable="true"></p></td>
+      </tr>
+      <tr>
+        <td colspan="5"><p>AP. CIRCULATÓRIO</p><p contenteditable="true"></p></td>
+        <td colspan="2"><p>ELETROCARDIOGRAMA</p><p contenteditable="true"></p></td>
+      </tr>
+      <tr>
+        <td colspan="3"><p>AP. DIGESTIVO</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>DENTES</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>PESCOÇO</p><p contenteditable="true"></p></td>
+        <td colspan="2"><p>AP. URINÁRIO</p><p contenteditable="true"></p></td>
+      </tr>
+      <tr>
+        <td colspan="3"><p>ESTADO MENTAL</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>ATARAXICOS</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>CORTICOIDES</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>ALERGIA</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>HIPOTENSORES</p><p contenteditable="true"></p></td>
+      </tr>
+      <tr>
+        <td colspan="5"><p>DIAGNÓSTICO PRÉ-OPERATÓRIO</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>ESTADO FÍSICO</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>RISCO</p><p contenteditable="true"></p></td>
+      </tr>
+      <tr>
+        <td colspan="7"><p>ANESTESIAS ANTERIORES</p><p contenteditable="true"></p></td>
+      </tr>
+      <tr>
+        <td colspan="3"><p>MEDICAÇÃO PRÉ ANESTÉSICA</p><p contenteditable="true"></p></td>
+        <td colspan="1"><p>APLICADA ÀS</p><p contenteditable="true"></p></td>
+        <td colspan="3"><p>EFEITO</p><p contenteditable="true"></p></td>
+      </tr>
+    </tbody>
+  </table>
+
+</main>
+
+<style>
+
+  main {
+    max-width: 900px;
+    margin: 0 auto;
+  }
+  header {
+    display: flex;
+    flex-direction: column;
+  }
+
+  header .logo {
+    max-width: 25%;
+  }
+
+  header h1 {
+    font-size: 1rem;
+  }
+
+  tr {
+    height: .5rem;
+  }
+
+  td {
+    padding: 2px 0 0 2px;
+  }
+
+  td p:nth-child(1) {
+    /* position: absolute; */
+    /* height: 14px; */
+    font-size: .5rem;
+    top: 0;
+    left: 2px;
+    word-break: break-all;
+    margin: 0;
+  }
+
+  td p:nth-child(2) {
+    font-size: 1rem;
+    top: 0;
+    left: 2px;
+    word-break: break-all;
+    margin: 0;
+    font-weight: 600;
+  }
+</style>
